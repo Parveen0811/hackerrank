f1cb5625707db9cc82f247646e66b4880c95e3da
Parveen0811 <152719148+Parveen0811@users.noreply.github.com>
2024-04-02 18:43:21 +0530

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,12 @@
+int main() {
+    int i;
+    long l;
+    char s;
+    float f;
+    double d;
+    
+    scanf("%d %ld %c %f %lf",&i,&l,&s,&f,&d);
+    
+    printf("%d \n %ld \n %c \n %f \n %lf",i,l,s,f,d);
+    return 0;
+}
